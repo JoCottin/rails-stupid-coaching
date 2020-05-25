629b2c16710e9a455a0a19bbe7cdd04d962b58ad
Joseph Cottin <joseph.cottin@kedgebs.com>
2020-05-25 15:33:16 +0200

    2nd exo done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @questions = params[:question]
+    if params[:question] == "I am going to work right now!"
+      @answer = ""
+    elsif params[:question].include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+    return @answer
+  end
+end
